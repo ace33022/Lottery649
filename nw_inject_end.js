9fd0dde769fa2e68918f20bb32d1b4cb5a4fdd07
小爪哇 <ace33022@gmail.com>
2018-09-21 22:23:50 +0800

    Update nw_inject_end.js

--- nw_inject_end.js
+++ nw_inject_end.js
@@ -431,7 +431,7 @@ Configurations.loadJS(Configurations.requirejsFile, function() {
 										var tag = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'
 														+ '<!-- ToolGroup -->'
 														// + '<ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-2244483882494685"	data-ad-slot="7390066670"	data-ad-format="auto" data-full-width-responsive="true"></ins>'
-														+ '<ins class="adsbygoogle" style="style="display: inline-block; width: 100%; height:90px;" data-ad-client="ca-pub-2244483882494685" data-ad-slot="7390066670" data-ad-format="auto" data-full-width-responsive="true"></ins>'
+														+ '<ins class="adsbygoogle" style="display: inline-block; width: 100%; height:90px;" data-ad-client="ca-pub-2244483882494685" data-ad-slot="7390066670" data-ad-format="auto" data-full-width-responsive="true"></ins>'
 														+ '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
 										
 										view.getContainer().find('tbody').parent().parent().scrollTop(view.getContainer().find('tbody').parent().parent()[0].scrollHeight);
